45b3ca39d3365fc07ef75fce53466b9fc49aa950
Fakiya Favour <fakiyaoluwatosin@gmail.com>
2025-03-05 16:23:01 +0100

    revert to view port for scalling height

--- app/contextual/page.tsx
+++ app/contextual/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function ContextualPage() {
   return (
     <main className=" relative w-full h-full flex flex-col  gap-y-12 pb-[5%] max-w-[130rem] bg-white shadow-[0px_1px_2px_0px_#09090B0D,0px_0px_0px_1px_#09090B0D] rounded-[32px]  p-3 md:p-6">
-      <section className="  grid  grid-cols-1 lg:grid-cols-2 min-h-full md:min-h-[95vh]  h-full   gap-6">
+      <section className="  grid  grid-cols-1 lg:grid-cols-2 min-h-[95vh]  h-full   gap-6">
         <div className=" w-full flex  gap-y-4 justify-center  lg:items-center  flex-col  rounded-lg p-4">
           <div className=" w-full md:w-[20rem] tracking-[0] ">
             <h2 className="text-base  text-grayMain font-medium ">
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main
       className=" relative max-w-[130rem] w-full space-y-6 items-center shadow-[0px_1px_2px_0px_#09090B0D,0px_0px_0px_1px_#09090B0D]
- rounded-[32px] grid grid-cols-1 md:grid-cols-2  min-h-full md:min-h-[90vh] max-h-[40rem] h-full bg-white p-6 lg:px-8 gap-6"
+ rounded-[32px] grid grid-cols-1 md:grid-cols-2 min-h-[90vh] max-h-[40rem] h-full bg-white p-6 lg:px-8 gap-6"
     >
       <section className="w-full md:w-[20rem] flex flex-col items-center justify-center gap-y-3 rounded-lg">
         <div className="flex flex-col gap-y-3 px-2 tracking-[0]">
